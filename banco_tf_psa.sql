0ae02224fc9e6c647f163e5708749d0af34c7f3a
lucas pedreira <lpssadglobal>
2017-11-19 21:47:05 -0200

    Banco Final

--- banco_tf_psa.sql
+++ banco_tf_psa.sql
@@ -18,21 +18,10 @@ CREATE SCHEMA IF NOT EXISTS `tf-psa` DEFAULT CHARACTER SET utf8 ;
 USE `tf-psa` ;
 
 -- -----------------------------------------------------
--- Table `tf-psa`.`leilao`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tf-psa`.`leilao` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `data_hora_inicio` DATETIME NULL,
-  `data_hora_fim` DATETIME NULL,
-  `valor_minimo` DECIMAL NULL,
-  `tipo` INT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `tf-psa`.`user`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `tf-psa`.`users`;
+
 CREATE TABLE IF NOT EXISTS `tf-psa`.`users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL DEFAULT '',
@@ -46,6 +35,9 @@ CREATE TABLE IF NOT EXISTS `tf-psa`.`users` (
   `is_deleted` tinyint(1) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 );
+
+DROP TABLE IF EXISTS `tf-psa`.`ci_sessions`;
+
 CREATE TABLE IF NOT EXISTS `tf-psa`.`ci_sessions` (
   `id` varchar(40) NOT NULL,
   `ip_address` varchar(45) NOT NULL,
@@ -55,59 +47,63 @@ CREATE TABLE IF NOT EXISTS `tf-psa`.`ci_sessions` (
   KEY `ci_sessions_timestamp` (`timestamp`)
 );
 
+INSERT INTO `users` VALUES (1,'admin','admin@admin.com','$2y$10$cnkKlBRMFVecOZXlZn6cAeDqaOIAHWvb98MI3tPO9l5QfGdyqRGs6','default.jpg','2017-11-18 20:57:11',NULL,0,0,0);
+
+-- -----------------------------------------------------
+-- Table `tf-psa`.`leilao`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `tf-psa`.`leilao`;
+
+CREATE TABLE IF NOT EXISTS `tf-psa`.`leilao` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `data_hora_inicio` DATETIME NULL,
+  `data_hora_fim` DATETIME NULL,
+  `valor_minimo` DECIMAL NULL,
+  `tipo` INT NULL,
+  `tipo_lance` INT NULL,
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
 -- -----------------------------------------------------
 -- Table `tf-psa`.`lance`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `tf-psa`.`lance`;
+
 CREATE TABLE IF NOT EXISTS `tf-psa`.`lance` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `data_hora` DATETIME NULL,
   `valor` DECIMAL NULL,
   `leilao_id` INT NOT NULL,
   `user_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_lance_leilao1_idx` (`leilao_id` ASC),
-  INDEX `fk_lance_user1_idx` (`user_id` ASC),
-  CONSTRAINT `fk_lance_leilao1`
-    FOREIGN KEY (`leilao_id`)
-    REFERENCES `tf-psa`.`leilao` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_lance_user1`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `tf-psa`.`user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `tf-psa`.`lance_tipo`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tf-psa`.`lance_tipo` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(45) NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
-
 
 -- -----------------------------------------------------
 -- Table `tf-psa`.`produto_categoria`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `tf-psa`.`produto_categoria`;
+
 CREATE TABLE IF NOT EXISTS `tf-psa`.`produto_categoria` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(45) NULL,
+  `user_id` INT NOT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
+
+INSERT INTO `produto_categoria` VALUES (1,'Eletronico',1),(2,'Mobilia',1),(3,'Veiculo',1);
 
 
 -- -----------------------------------------------------
 -- Table `tf-psa`.`lote`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `tf-psa`.`lote`;
+
 CREATE TABLE IF NOT EXISTS `tf-psa`.`lote` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(45) NULL,
-  `leilao_id` INT NOT NULL,
+  `leilao_id` INT,
+  `user_id` INT NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_lote_leilao1_idx` (`leilao_id` ASC),
   CONSTRAINT `fk_lote_leilao1`
@@ -117,16 +113,21 @@ CREATE TABLE IF NOT EXISTS `tf-psa`.`lote` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+INSERT INTO `lote` VALUES (1,'lote 1',NULL,1),(2,'lote 2',NULL,1),(3,'lote 3',NULL,1);
+
 
 -- -----------------------------------------------------
 -- Table `tf-psa`.`produto`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `tf-psa`.`produto`;
+
 CREATE TABLE IF NOT EXISTS `tf-psa`.`produto` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `descricao_breve` VARCHAR(45) NULL,
   `descricacao_completa` TEXT NULL,
   `produto_categoria_id` INT NOT NULL,
-  `lote_id` INT NOT NULL,
+  `lote_id` INT,
+  `user_id` INT NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_produto_produto_categoria_idx` (`produto_categoria_id` ASC),
   INDEX `fk_produto_lote1_idx` (`lote_id` ASC),
@@ -134,13 +135,10 @@ CREATE TABLE IF NOT EXISTS `tf-psa`.`produto` (
     FOREIGN KEY (`produto_categoria_id`)
     REFERENCES `tf-psa`.`produto_categoria` (`id`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_produto_lote1`
-    FOREIGN KEY (`lote_id`)
-    REFERENCES `tf-psa`.`lote` (`id`)
-    ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
+
+INSERT INTO `produto` VALUES (1,'Celular','Teste',1,NULL,1),(2,'TV','64 polegadas',1,1,1),(5,'Fogão','6 bocas',1,2,1);
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
